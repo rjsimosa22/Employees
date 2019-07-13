3f31395e347276a9ee78a4d89b8eddafd55819f5
rjsimosa22 <rjsimosa.cultca@gmail.com>
2019-07-13 11:16:56 -0500

    listo

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,37 @@ Route::get('/', function () {
     return view('acces');
 });
 
-Route::resource('tasks', 'TaskController');
 Route::get('/home', 'HomeController@index')->name('home');
 
+// Users
+Route::resource('user', 'UsersController');
+Route::resource('listUser', 'UsersController');
+Route::post('view', 'UsersController@models');
+Route::post('listUser/store', 'UsersController@store');
+Route::get('listUser/delete/{id}', 'UsersController@destroy');
+
+// Vehicles
 Route::resource('vehicles', 'VehiclesController');
 Route::post('models', 'VehiclesController@models');
 Route::resource('listVehicles', 'VehiclesController');
 Route::post('listVehicles/store', 'VehiclesController@store');
 Route::get('listVehicles/delete/{id}', 'VehiclesController@destroy');
 
+// Clients
 Route::resource('client', 'ClientController');
 Route::post('search', 'ClientController@search');
 Route::resource('listClient', 'ClientController');
 Route::post('listClient/store', 'ClientController@store');
 Route::get('listClient/delete/{id}', 'ClientController@destroy');
+
+// System
+Route::post('add', 'SystemController@add');
+Route::resource('system', 'SystemController');
+Route::resource('listSystem', 'SystemController');
+Route::post('listSystem/store', 'SystemController@store');
+Route::get('listSystem/delete/{id}', 'SystemController@destroy');
+
+
 
 
 
